887202b150f119c78dd5dde6c47af73a03ed5e89
Marek <marciu1994@gmail.com>
2012-10-01 19:02:47 +0200

    Reader 1.0.0.2

---
+++ Reader/Reader/Engine.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Reader
+{
+    public class Engine
+    {
+        public static DateTime PauseForMilliSeconds(int MilliSecondsToPauseFor)
+        {
+
+
+            System.DateTime ThisMoment = System.DateTime.Now;
+            System.TimeSpan duration = new System.TimeSpan(0, 0, 0, 0, MilliSecondsToPauseFor);
+            System.DateTime AfterWards = ThisMoment.Add(duration);
+
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = System.DateTime.Now;
+            }
+
+
+            return System.DateTime.Now;
+        }
+        
+        public int SleepMsEachLetter = 5;
+        public int CharactersPerMinute = 2000;
+        public int SleepTimeBasic = 50;
+
+
+        internal void Read(string text, TextBox viewer)
+        {
+            if (text == string.Empty || text == null)
+                return;
+
+            Console.Write("Znaków: {0}", text.Length);
+            string[] toRead = text.Split(' ');
+            Console.WriteLine(", w tym słów: {0}", toRead.Length);
+
+            int wordsLeft = 0;
+            int sleepTime = 0;
+            
+
+            for (int i = 0; i < toRead.Length; ++i)
+            {
+                SleepMsEachLetter = 60 * 1000 / CharactersPerMinute;
+                sleepTime = 0;
+                wordsLeft = toRead.Length - i;
+
+                //if (wordsLeft >= 3)
+                //{
+                //    viewer.Text = toRead[i] + ' ' + toRead[i + 1] + ' ' + toRead[i + 2] + ' ';
+                //    sleepTime += (toRead[i].Length + toRead[i + 1].Length + toRead[i + 2].Length) * SleepMsEachLetter + SleepTimeBasic;
+                //}
+                //else if (wordsLeft >= 2)
+                //{
+                //    viewer.Text = toRead[i] + ' ' + toRead[i + 1] + ' ';
+                //    sleepTime += (toRead[i].Length + toRead[i + 1].Length) * SleepMsEachLetter + SleepTimeBasic;
+                //}
+                if (wordsLeft >= 1)
+                {
+                    viewer.Text = toRead[i] + ' ';
+                    sleepTime += (toRead[i].Length) * SleepMsEachLetter + SleepTimeBasic;
+                }
+                else if (wordsLeft == 0)
+                    break;
+                else break;
+
+                PauseForMilliSeconds(sleepTime);
+            }
+            
+        }
+    }
+}
---
+++ Reader/Reader/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace Reader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Tray icon (book)
+        /// </summary>
+        private TaskbarIcon taskBarIcon = new TaskbarIcon();
+
+        private Engine engine = new Engine();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Tray icon
+            taskBarIcon.Icon = (System.Drawing.Icon)Properties.Resources.book;
+            taskBarIcon.ToolTipText = "Enditu Reader";
+            taskBarIcon.Visibility = System.Windows.Visibility.Collapsed;
+            taskBarIcon.TrayLeftMouseUp += taskBarIcon_TrayLeftMouseUp;
+
+            // TODO awaiting for activity
+
+
+            // TODO reading methods choice
+
+            // TODO reading methods
+        }
+
+
+
+        /// <summary>
+        /// Tray icon clicked event
+        /// </summary>
+        void taskBarIcon_TrayLeftMouseUp(object sender, RoutedEventArgs e)
+        {
+            this.Show();
+            this.WindowState = System.Windows.WindowState.Normal;
+            taskBarIcon.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Change window state event
+        /// </summary>
+        private void Window_StateChanged_1(object sender, EventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case System.Windows.WindowState.Minimized:  // Minimalizowanie
+                    taskBarIcon.Visibility = System.Windows.Visibility.Visible;
+                    this.Hide();
+                    break;
+                case System.Windows.WindowState.Maximized:  // Maksymalizowanie
+                case System.Windows.WindowState.Normal:     // Normalne okno
+                    this.Show();
+                    taskBarIcon.Visibility = System.Windows.Visibility.Collapsed;
+                    break;
+            }
+        }
+        
+        /// <summary>
+        /// Read button click event
+        /// </summary>
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+            engine.Read(Clipboard.GetText(), TBRead);
+        }
+
+        private void SliderPerLetterDelay_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (this.IsInitialized)
+            {
+                int temp = (int)Math.Round((double)e.NewValue, 0);
+                LabelPerLetterDelay.Content = temp.ToString();
+                engine.CharactersPerMinute = temp;
+            }
+        }
+    }
+}
